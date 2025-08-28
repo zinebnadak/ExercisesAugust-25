fcad8c9b4123e2d7657d07590572f61ea0f28480
zinebnadak <zinebnadak04@outlook.com>
2025-08-28 09:54:12 +0300

    changes 28.8

--- #1Exercises.py
+++ #1Exercises.py
@@ -53,3 +53,88 @@ elif minutes >= 33 and minutes <=66:                #use "and" not "or" to get i
     print ("Buy a Normal package!")
 elif minutes > 66:
     print ("Buy a Plus package!")
+
+#Write a program that reads in integers n and calculates sum, 1+2+3+4+...+n
+print()
+n = int(input("Enter value for n: "))
+sum = 0  #the sum is 0 in beginning
+k = 1  #start from number 1, k is used to keep track of the current number in summation
+
+while k <= n:
+    #the new values of sum and k after each loop
+    sum = sum + k  #adds value of k to sum
+    k= k+1         #then add k (1) for next time
+print ("The sum is:", sum)
+
+#Write a program that reads in integers n and calculates sum, 1+4+9+16+...+n^2
+print()
+n = int(input("Enter value of n: "))
+
+sum = 0
+k = 1
+
+while k <= n:
+#new values for sum and k:
+    sum=sum+(k*k)           #add square of k to the current value of sum)
+    k=k+1
+print ("The sum is", sum)
+
+#Suppose a ball is dropped onto a floor and with each bounce it loses 30% of its height. Write a program that calculates how many times such a ball bounces before it comes to rest. (By “comes to rest,” we mean that it no longer bounces higher than 1 cm.) As input, the program should let the user specify the height, measured in meters, from which the ball is dropped.
+height = float(input("Enter the height you are trowing the ball from (in meters):" ))
+
+start_height = height   # Save the initial height before it changes to use it in print at the end
+times_bounced = 0  #start from 0 bounced
+
+while height >= 0.01:      #keep looping and register bounces and stop when height is 1 cm
+    times_bounced = times_bounced +1
+    height = height*0.7
+
+print (f"If you drop a ball from {start_height} meters it will bounce {times_bounced} times, before stopping. ")
+
+
+#Write a program that reads in the last integer( n) and calculates sum, 1+4+9+16+...+n^2, but this time use for-loop instead of while loop.
+
+#iterate trough the numbers from 1 to n
+#square each number
+#add it to a running total
+print ()
+
+n = int(input("Enter the value of n: "))
+
+sum = 0
+for i in range (1,n+1,1):               #range(1, n+1) The sequence will go up to but not including n+1. This ensures that n is included in the sequence., and by default setpes is 1
+    sum= sum+(i*i)
+
+print(f"The sum of squares from 1 to {n} is: {sum}")
+
+#the program calculate the value of the expression 2*(x**2)-5*x+1 for each value of x in the range -10 to 10, and prints result in a neat way.
+print()
+print()
+
+print (f"{"x":<4}{"Result":<10}")   #Left-align columns
+for x in range (-10,11,1):
+   result = 2*(x**2)-5*x+1
+   print (f"{x:<4} {result:<10}")    # Print the result in a table format
+print()
+print()
+
+# increment value of x by 0.1 (a float) in range -1 to 1
+# because value increments by 0.1 (float) we will use a while loop and start from -1 instead of a for loop
+print (f"{"x":<4}{"Result":<10}")   #Left-align columns
+
+x= -1 #start at -1
+while x <=1:      #The range() function only accepts integer values for start, stop, and step. # while will Loop until x reaches 1
+   result = 2*(x**2)-5*x+1
+   print (f"{x:<4.1f} {result:<10.2f}")    # Print the result in a table format
+   x = x+0.1        # Increment x by 0.1
+
+#Nested loops: program that lets user choose number of rows that are going to be printed and for every row prints one more "+" sign is going to be printed, starting with one "+" sign at row one.
+
+rows = int(input("Enter desired number of rows: "))
+
+for i in range (1,rows+1,1):
+    for j in range (1,i+1,1):
+        print("+",end="")
+    print ()    #terminates row
+
+#Now reverse it : desired number of rows = number of "+" signs to be printed on first line
