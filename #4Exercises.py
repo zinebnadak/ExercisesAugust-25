a0295a632a07b7ee6516d695b36db0e6405baef8
zinebnadak <zinebnadak04@outlook.com>
2025-08-29 08:56:55 +0300

    changes 29.8

---
+++ #4Exercises.py
@@ -0,0 +1,5 @@
+#write a program that handles text. The program will determine if the texts first letter is the same as the texts last letter
+
+#from sequence "pytonorm", select "ton", "norm" and "not" by using slicing.
+
+#
